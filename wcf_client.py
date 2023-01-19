a2fc019b713ea7c09a7d5b2482ba612bee7e4de8
ZJUStarStar <zjustarstar@gmail.com>
2023-01-19 16:06:32 +0800

    相机数据通过wcf服务获取

--- wcf_client.py
+++ wcf_client.py
@@ -1,25 +1,37 @@
 # -*- coding: utf-8 -*-
 from suds.client import Client
-from config import CFG
-import json
-
+import config as CFG
+import logging
 
 
 def SetCameraParams(exposure, gainpow):
-    client = Client(CFG.WCF_SERVER_ADDRESS)
-    # print(client)
-    result = client.service.SetMicAndGetPic(exposure, gainpow)
-    print(result)
+    logger = logging.getLogger(CFG.LOG_NAME)
+
+    try:
+        client = Client(CFG.WCF_SERVER_ADDRESS)
+        # print(client)
+
+        result = client.service.SetMicAndGetPic(exposure, gainpow)
+        logger.info("访问WCF服务SetMicAndGetPic返回结果:%s", result)
+        # print(result)
+    except Exception as e:
+        logger.info("访问WCF服务SetMicAndGetPic出错: %s", e)
+        print("Error:", e)
 
     return result
 
 
 def GetPicture():
-    client =Client(CFG.WCF_SERVER_ADDRESS)
-    # print(client)
-    result = client.service.GetPicture()
-    print(result)
+    logger = logging.getLogger(CFG.LOG_NAME)
+    try:
+        client = Client(CFG.WCF_SERVER_ADDRESS)
+        # print(client)
+        result = client.service.GetPicture()
+        logger.info("访问WCF服务GetPicture返回结果:%s", result)
+        # print(result)
+    except Exception as e:
+        logger.info("访问WCF服务GetPicture出错: %s", e)
+        print("Error:", e)
+        return None
 
-    # jsonStr =json.load(result)
-    # print(jsonStr)
     return result
